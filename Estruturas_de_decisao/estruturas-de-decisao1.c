082556d121d41bd88726233c4f3c6c53675bc0de
David-DEV2025 <dfreitas.adm@outlook.com>
2025-09-23 19:40:18 -0300

    Estruturas de Decisão

---
+++ Estruturas_de_decisao/estruturas-de-decisao1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+
+    int condicao1, condicao2;
+
+    //estrutura aninhada
+    if (condicao1) {
+        if (condicao2){
+            // Código a ser executado se condicao1 e condicao2 forem verdadeiras
+        }
+    }
+}
---
+++ Estruturas_de_decisao/estruturas-de-decisao5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    int nota;
+
+    printf("Digite a sua nota: ");
+    scanf("%d", &nota);
+
+    // A >= 90
+    // B >= 80
+    // C >= 70
+    // D >= 60
+    // E >= 50
+    // F
+
+    if (nota >= 90){
+        printf("O conceito A!\n");
+    } else if (nota >= 80){
+        printf("O conceito é B!\n");
+    } else if (nota >= 70){
+        printf("O conceito é C!\n");
+    } else if (nota >= 60){
+        printf("O conceito é D!\n");
+    } else if (nota >= 50){
+        printf("O conceito é E!\n");
+    } else {
+        printf("O conceito é F!\n");
+    }
+}
